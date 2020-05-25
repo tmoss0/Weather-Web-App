1a4ceb1804b227063cdc83b7bf0693e9ff265011
Tim Moss <tmoss726@hotmail.com>
2020-05-25 12:10:02 -0400

    Update: store city/state, updateable location

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+// Initialize local storage
+const storage = new Storage();
+
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Initialize weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Initialize UI
 const ui = new UI();
 
 // Get weather on DOM load
-document.addEventListener('DOMContentLoaded', getWeather)
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+	const city = document.getElementById('city').value;
+	const state = document.getElementById('state').value;
+
+	// Change location
+	weather.changeLocation(city, state);
+
+	// Set location in local storage
+	storage.setLocationData(city, state);
+
+	// Get and display weather
+	getWeather();
+	
+	// Close modal
+	$('#locModal').modal('hide');
+});
 
 function getWeather() {
 	weather.getWeather()
